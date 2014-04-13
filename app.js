b2293a8985dd43a2190d4ad685e8af0d90f2e8a1
R Prakash <prak5190@yahoo.co.in>
2014-04-14 01:02:07 +0530

    XHR for Fileupload - FormData

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.get('/fileUpload', fup.fileUpload);
+app.all('/fileUpload', fup.fileUpload);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -6,6 +6,9 @@
  * GET users listing.
  */
 
-exports.fileUpload = function(req, res){
-	res.render('index', { title: 'Express' });
+exports.fileUpload = function(req, res){		
+	//res.render('index', { title: 'Express' });
+	console.dir(req);
+	res.write("Done");
+	res.end("");
 };
